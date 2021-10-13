cbaaf6dec645fa2c5df0a98a2e42f5e0dfb839c8
Alexdelia <alexandre.delille.57@gmail.com>
2021-10-13 12:47:43 +0200

    close to real code ft_render

--- srcs/ft_render.c
+++ srcs/ft_render.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 12:19:02 by adelille          #+#    #+#             */
-/*   Updated: 2021/10/13 12:27:39 by adelille         ###   ########.fr       */
+/*   Updated: 2021/10/13 12:47:23 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,32 @@ void	ft_pixel(t_img *img, int color, int index)
 	img->buffer[index + 1] = color >> 8 & 0xFF;
 	img->buffer[index + 2] = color >> 16 & 0xFF;
 	img->buffer[index + 3] = 0;
+}
+
+int	ft_render(xxx)
+{
+	int	x;
+	int	y;
+	int	index;
+
+	env->img.addr = mlx_new_image(env->mlx, env->size_x, env->size_y);
+	env->img.buffer = mlx_get_data_addr(env->img.addr, &env->img.bpp,
+			&env->img.line_size, &env->img.endian);
+	index = 0;
+	y = 0;
+	while (y < env->size_y)
+	{
+		x = 0;
+		while (x < env->size_x)
+		{
+			// if julia, launch julia
+			ft_pixel(env, ft_julia, index);
+			index += 4;
+			x++;
+		}
+		y++;
+		printf("\rRender: [%d%%]", i + 1, (y * 100) / env->size_y);
+	}
+	printf("		\033[1;32mDone\033[0m\033[1m\n");
+	return (TRUE); //
 }
--- srcs/julia.c
+++ srcs/julia.c
@@ -6,7 +6,7 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 15:37:46 by adelille          #+#    #+#             */
-/*   Updated: 2021/10/12 15:59:57 by adelille         ###   ########.fr       */
+/*   Updated: 2021/10/13 12:42:36 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,10 +61,37 @@ int	ft_julia(t_env *env)
 			color.g = 255;
 			color.b = 255 * (i < MAX_ITERATION);
 			color.shadow = //
-			// set pixel as color
-			ft_xxx(x, y, color);
-			x++;
+				// set pixel as color
+				ft_pixel(env->img,), 
+				x++;
 		}
 		y++;
 	}
+}
+
+int	ft_julia(env->img, index)
+{
+	int		i;
+	double	newR; // will be struct
+	double	newIm;
+	double	oldR;
+	double	oldIm;
+
+	// tmp
+	double	zoom = 1, move_x = 0, move_y = 0; // will be get from input
+
+	newR = 1.5 * (x - env->size_x / 2) / (0.5 * zoom * env->size_x) + move_x;
+	newIm = (y - env->size_y / 2) / (0.5 * zoom * env->size_y) + move_y;
+	i = 0;
+	while (i < MAX_ITERATION)
+	{
+		oldR = newR;
+		oldIm = newIm;
+		newR = oldR * oldR - oldIm * oldIm + C_R;
+		newIm = 2 * oldR * oldIm + C_I;
+		if ((newR * newR + newIm * newIm) > 4)
+			break ;
+		i++;
+	}
+	return (ft_ints_to_int(i % 256, 255, 255 * (i < MAX_ITERATION), 0));
 }
