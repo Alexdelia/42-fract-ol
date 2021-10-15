8a874b66d71f5ab575e8719dcd0248adfe097094
Alexdelia <alexandre.delille.57@gmail.com>
2021-10-15 22:47:13 +0200

    implementing mandelbrot set, WIP

--- srcs/mandelbrot.c
+++ srcs/mandelbrot.c
@@ -6,11 +6,16 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 21:09:06 by adelille          #+#    #+#             */
-/*   Updated: 2021/10/15 21:16:09 by adelille         ###   ########.fr       */
+/*   Updated: 2021/10/15 22:41:48 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fo.h"
+
+#define	MIN_R	-1.5
+#define	MAX_R	0.7
+#define	MIN_I	-1.0
+#define MAX_I	1.0
 
 int	ft_mandelbrot(t_env *env, int x, int y)
 {
@@ -18,17 +23,17 @@ int	ft_mandelbrot(t_env *env, int x, int y)
 	double		tmp;
 	int			i;
 
-	c.r0 = x;
-	c.i0 = y;
+	c.r0 = x * (MAX_R - MIN_R / SIZE_X) + MIN_R;
+	c.i0 = y * (MAX_I - MIN_I / SIZE_Y) + MIN_I;
 	c.r1 = 0;
 	c.i1 = 0;
 	i = 0;
 	while (c.r1 * c.r1 + c.i1 * c.i1 < 4 && i < env->ite)
 	{
 		tmp = c.r1 * c.r1 - c.i1 * c.i1 + c.r0;
-		c.i1 = 2 * c.r1 * c.r1 + c.i0;
+		c.i1 = 2 * c.r1 * c.i1 + c.i0;
 		c.r1 = tmp;
 		i++;
 	}
-	return (ft_ints_to_int(255 - 255 * ((env->ite - i) * (env->ite - i)) % (env->ite * env->ite), 0, 0));
+	return (ft_ints_to_int(i % 256, i % 256, i % 256));
 }
