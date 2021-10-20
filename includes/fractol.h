748cbf323482cbb8ba56170b7ae310b3d6e633d0
Alexdelia <alexandre.delille.57@gmail.com>
2021-10-20 20:15:42 +0200

    norm .h

--- includes/fractol.h
+++ includes/fractol.h
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fo.h                                               :+:      :+:    :+:   */
+/*   fractol.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 18:49:16 by adelille          #+#    #+#             */
-/*   Updated: 2021/10/19 20:34:31 by adelille         ###   ########.fr       */
+/*   Updated: 2021/10/20 20:15:22 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	FRACTOL_H
+#ifndef FRACTOL_H
 # define FRACTOL_H
 
 # include "../libft/includes/libft.h"
@@ -36,7 +36,7 @@
 # define REAL			-0.7
 # define IMAGINARY		0.27015
 
-typedef struct	s_img
+typedef struct s_img
 {
 	void		*addr;
 	char		*buffer;
@@ -45,7 +45,7 @@ typedef struct	s_img
 	int			endian;
 }				t_img;
 
-typedef struct	s_env
+typedef struct s_env
 {
 	void		*mlx;
 	void		*win;
@@ -62,7 +62,7 @@ typedef struct	s_env
 	t_img		*img;
 }				t_env;
 
-typedef struct	s_color
+typedef struct s_color
 {
 	double		r;
 	double		g;
@@ -70,7 +70,7 @@ typedef struct	s_color
 	int			shadow;
 }				t_color;
 
-typedef struct	s_complex
+typedef struct s_complex
 {
 	double		cr;
 	double		ci;
@@ -78,23 +78,23 @@ typedef struct	s_complex
 	double		zi;
 }				t_complex;
 
-int		ft_arg(t_env *env, int ac, char **av);
-int		ft_has_help(int ac, char **av);
-int		ft_option(void);
+int			ft_arg(t_env *env, int ac, char **av);
+int			ft_has_help(int ac, char **av);
+int			ft_option(void);
 
-int		ft_render(t_env *env);
-int		ft_display(t_env *env);
+int			ft_render(t_env *env);
+int			ft_display(t_env *env);
 
-int		ft_julia(t_env *env, int x, int y);
-int		ft_mandelbrot(t_env *env, int x, int y);
+int			ft_julia(t_env *env, int x, int y);
+int			ft_mandelbrot(t_env *env, int x, int y);
 
-int		ft_ints_to_int(int r, int g, int b);
-int		ft_adjust_int(int color);
-int		ft_red_to_black(int color);
+int			ft_ints_to_int(int r, int g, int b);
+int			ft_adjust_int(int color);
+int			ft_red_to_black(int color);
 
-int		ft_free_exit(t_params *p);
-int		ft_is_double(const char *n);
-int		ft_is_num(const char *n);
+int			ft_free_exit(t_params *p);
+int			ft_is_double(const char *n);
+int			ft_is_num(const char *n);
 t_complex	ft_init_complex(double real, double imaginary);
 
 #endif
